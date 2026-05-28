9f78ab0a6ec016e9f03f61a6ce922e8c98b019fa
nikhil-sharma-dotcom <nv55872@gmail.com>
2026-05-28 10:31:41 +0530

    Initial commit - ecommerce API project

---
+++ cmd/api.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	repo "github.com/sikozonpc/ecom/internal/adapters/postgresql/sqlc"
+	"github.com/sikozonpc/ecom/internal/orders"
+	"github.com/sikozonpc/ecom/internal/products"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP)    // import for rate limiting and analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+	productService := products.NewService(repo.New(app.db))
+	productHandler := products.NewHandler(productService)
+	r.Get("/products", productHandler.ListProducts)
+
+	orderService := orders.NewService(repo.New(app.db), app.db)
+	ordersHandler := orders.NewHandler(orderService)
+	r.Post("/orders", ordersHandler.PlaceOrder)
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+	// logger
+	db *pgx.Conn
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/sikozonpc/ecom/internal/env"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cfg := config{
+		addr: ":8080",
+		db: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
+		},
+	}
+
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	// Database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+
+	api := application{
+		config: cfg,
+		db: conn,
+	}
+	if err := api.run(api.mount()); err != nil {
+		slog.Error("server failed to start", "error", err)
+		os.Exit(1)
+	}
+}
